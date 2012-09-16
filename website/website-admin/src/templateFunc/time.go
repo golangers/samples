61cbf54d5f08609f453b79624c00e297447e8ff5
leetaifook <lee@leetaifook.com>
2012-09-16 23:43:08 +0800

    Add tempalateFunc of time

---
+++ website/website-admin/src/templateFunc/time.go
@@ -0,0 +1,12 @@
+package templateFunc
+
+import (
+	. "controllers"
+	"golanger.com/framework/utils"
+)
+
+func init() {
+	App.AddTemplateFunc("GetTimeToStr", func(tm int64) string {
+		return utils.NewTime().GetTimeToStr(tm)
+	})
+}
