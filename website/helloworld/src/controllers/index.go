495c38e003a3290a1f8c6ed1bfd2503feb750efc
leetaifook <lee@leetaifook.com>
2012-10-23 13:15:08 +0800

    Add FilterAction feature

--- website/helloworld/src/controllers/index.go
+++ website/helloworld/src/controllers/index.go
@@ -14,6 +14,39 @@ func init() {
 	App.RegisterController("index/", PageIndex{})
 }
 
+func (p *PageIndex) Before_() []map[string]string {
+	return []map[string]string{
+		map[string]string{
+			"_FILTER": "getIndex",
+			"_ALL":    "allow",
+			"Index":   "deny",
+		},
+		map[string]string{
+			"_FILTER":  "GetTest",
+			"TestPage": "allow",
+		},
+		map[string]string{
+			"_FILTER":  "GetTest1",
+			"TestPage": "allow",
+			"Index":    "allow",
+		},
+	}
+}
+
+func (p *PageIndex) After_() []map[string]string {
+	return []map[string]string{
+		map[string]string{
+			"_FILTER":  "after",
+			"_ALL":     "allow",
+			"TestPage": "deny",
+		},
+	}
+}
+
+func (p *PageIndex) After_Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("After_Index")
+}
+
 func (p *PageIndex) Index(w http.ResponseWriter, r *http.Request) {
 	p.SESSION["String"] = "String"
 	p.SESSION["string"] = "string"
@@ -36,4 +69,21 @@ func (p *PageIndex) TestPage(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("%v", p.SESSION["Int"])))
 	w.Write([]byte(fmt.Sprintf("%v", p.SESSION["Map"])))
 	w.Write([]byte(fmt.Sprintf("%v", p.SESSION["Int64"])))
+}
+
+func (p *PageIndex) Filter_getIndex() {
+	fmt.Println("GetIndex")
+}
+
+func (p *PageIndex) Filter_GetTest(r *http.Request) {
+	fmt.Println("GetTest")
+	fmt.Println(r.URL)
+}
+
+func (p *PageIndex) Filter_GetTest1() {
+	fmt.Println("GetTest1")
+}
+
+func (p *PageIndex) Filter_after() {
+	fmt.Println("Filter_after")
 }
