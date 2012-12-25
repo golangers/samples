071a6be040fb7e504bc4a8f6908eff704242ad62
leetaifook <lee@leetaifook.com>
2012-12-26 02:05:36 +0800

    Update api server

--- website/api/src/main.go
+++ website/api/src/main.go
@@ -4,24 +4,19 @@ import (
 	. "controllers"
 	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
 	"runtime"
 )
 
 var (
 	addr      = flag.String("addr", ":80", "Server port")
-	configDir = flag.String("config", "./config", "Directory of config")
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU()*2 - 1)
 
 	flag.Parse()
-	os.Chdir(filepath.Dir(os.Args[0]))
 	fmt.Println("Listen server address: " + *addr)
-	fmt.Println("Read configuration directory success, directory: " + filepath.Join(filepath.Dir(os.Args[0]), *configDir))
 
-	App.Load(*configDir)
+	App.LoadData(``)
 	App.ListenAndServe(*addr, App)
 }
